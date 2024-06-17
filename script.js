07d096d74f7389f07539b89c74c6ccfaef1f7ab3
Thaise Neri <thaise.silva1316@gmail.com>
2024-06-17 09:54:44 -0300

    aula1

---
+++ script.js
@@ -0,0 +1,20 @@
+//variáveis
+// var let
+//camelCase
+
+//let nome = 'Thaise'
+//let idade = 34
+//console.log(nome, age)
+
+let primeiroNome = 'Thaise'
+primeiroNome = 'Cristina'
+let idade
+console.log(primeiroNome, idade)
+
+const caltax = 
+console.log(caltax)
+
+let nome = 'Thaise'
+let sobrenome = 'Cristina'
+let nomeCompleto = nome + ' ' + sobrenome
+console.log(nomeCompleto)
